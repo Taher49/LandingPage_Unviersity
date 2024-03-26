6a2cf9b2b3796f42702cf6d22ab69a2670c504da
Taher49 <taherelbouhtouri49@gmail.com>
2024-03-26 17:11:56 +0000

    fix some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 
 
-import Navbar from "./Components/Navbar"
-import Hero from "./Components/Hero"
-import Cards from "./Components/cards"
-import Card from "./Components/Card"
-import About from './Components/About'
-import Next from './Components/Next'
-import Contactus from './Components/Contactus'
-import Accordion from './Components/Accordion'
-import Footer from './Components/footer'
+import Navbar from "./Components/navbar";
+import Hero from "./Components/Hero";
+import Cards from "./Components/cards";
+import About from './Components/About';
+import Next from './Components/Next';
+import Contactus from './Components/Contactus';
+import Accordion from './Components/Accordion';
+import Footer from './Components/footer';
 
 const App = () => {
 
